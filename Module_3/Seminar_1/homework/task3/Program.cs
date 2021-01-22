62fd13b1d4139ad52d829e8f7e8d362104be8224
doritosxxx <doritosxxx@users.noreply.github.com>
2021-01-22 20:13:54 +0400

    Add seminar 1 homework

---
+++ Module_3/Seminar_1/homework/task3/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+
+namespace task3
+{
+    class TemperatureConverterImp
+    {
+        public double FahrenheitToCelsius(double fahrenheit)
+        {
+            return (fahrenheit - 32) * 5 / 9;
+        }
+
+        public double CelsiusToFahrenheit(double celsius)
+        {
+            return 9 * celsius / 5 + 32;
+        }
+    }
+
+    static class StaticTempConverts
+    {
+        public static double CelsiusToKelvin(double celsius)
+        {
+            return celsius + 273.15;
+        }
+
+        public static double CelsiusToRankin(double celsius)
+        {
+            return (celsius + 273.15) * 9 / 5;
+        }
+
+        public static double CelsiusToReaumur(double celsius)
+        {
+            return celsius * 4 / 5;
+        }
+
+
+        public static double KelvinToCelsius(double kelvin)
+        {
+            return kelvin - 273.15;
+        }
+
+        public static double RankinToCelsius(double rankin)
+        {
+            return rankin * 5 / 9 - 273.15;
+        }
+
+        public static double ReaumurToCelsius(double reaumur)
+        {
+            return reaumur * 5 / 9 ;
+        }
+
+    }
+
+    class Program
+    {
+        delegate double delegateConvertTemperature(double degrees);
+        static void Main(string[] args)
+        {
+            // Main task.
+            TemperatureConverterImp temperatureConverterImp = new TemperatureConverterImp();
+
+            delegateConvertTemperature FahrenheitToCelsius = temperatureConverterImp.FahrenheitToCelsius;
+            delegateConvertTemperature CelsiusToFahrenheit = temperatureConverterImp.CelsiusToFahrenheit;
+
+            Console.WriteLine(FahrenheitToCelsius(1));
+            Console.WriteLine(CelsiusToFahrenheit(1));
+
+            // Subtasks.
+            Console.WriteLine("\nSubtasks");
+
+            delegateConvertTemperature[] converters = {
+                temperatureConverterImp.FahrenheitToCelsius,
+                temperatureConverterImp.CelsiusToFahrenheit
+            };
+
+            foreach (var function in converters)
+                Console.WriteLine(function(1));
+
+            delegateConvertTemperature[] convertersFromCelsius = {
+                temperatureConverterImp.CelsiusToFahrenheit,
+                StaticTempConverts.CelsiusToKelvin,
+                StaticTempConverts.CelsiusToRankin,
+                StaticTempConverts.CelsiusToReaumur
+            };
+
+
+            double celsius;
+
+            Console.WriteLine("Enter temperature in celsius:");
+            while (true)
+            {
+                if (double.TryParse(Console.ReadLine(), out celsius))
+                    break;
+                Console.WriteLine("Provided input is not a double");
+            }
+
+            foreach(var converter in convertersFromCelsius)
+                Console.WriteLine($"{converter.Method.Name}: {converter(celsius)}");
+        }
+    }
+}
---
+++ Module_3/Seminar_1/homework/task4/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace task4
+{
+    class Robot
+    {
+        int x, y;
+
+        public void Right() { x++; }
+        public void Left() { x--; }
+        public void Forward() { y++; }
+        public void Backward() { y--; }
+
+        public string Position()
+        {
+            return $"The Robot position: x={x}, y={y}";
+        }
+    }
+    class Program
+    {
+        delegate void Steps();
+
+        static void Main(string[] args)
+        {
+            Robot robot = new Robot();
+
+            // Main task.
+
+            string encodedPath = Console.ReadLine();
+            Steps path = null;
+
+            foreach (char command in encodedPath)
+                path += command switch {
+                    'R' => robot.Right,
+                    'L' => robot.Left,
+                    'F' => robot.Forward,
+                    'B' => robot.Backward,
+                    _   => throw new ArgumentException("bruh")
+                };
+
+            Console.WriteLine(robot.Position());
+            path();
+            Console.WriteLine(robot.Position());
+
+        }
+    }
+}
